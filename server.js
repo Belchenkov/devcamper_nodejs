b64b26cf0d4e0a7934973b0d795c4a2944d2098e
Aleksey Belchenkov <u608110@gmail.com>
2019-12-11 06:50:23 +0300

    Connecting to the Database with Mongoose

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
+// Connect DB
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
 // Route files
 const bootcamps = require('./routes/bootcamps');
 // Middleware
 //const logger = require('./middleware/logger');
-
-// Load env vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -24,4 +29,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
