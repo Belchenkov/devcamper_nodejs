a6d3b2c1a37e4f80b15ae8a7098a1fecad93a38c
Aleksey Belchenkov <u608110@gmail.com>
2019-12-26 07:43:17 +0300

    Errors

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,7 +14,7 @@ exports.getBootcamps = async (req, res, next) => {
             data: bootcamps
         });
     } catch (err) {
-        next(new ErrorResponse(`Bootcamps not found`));
+        next(err);
     }
 };
 
@@ -25,14 +25,14 @@ exports.getBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
-        if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));;
+        if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 
         res.status(200).json({
             success: true,
             data: bootcamp
         });
     } catch (err) {
-        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        next(err);
     }
 };
 
@@ -62,7 +62,7 @@ exports.updateBootcamp = async (req, res, next) => {
             runValidators: true
         });
 
-        if (!bootcamp) return res.status(400).json({ success: false });
+        if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 
         res.status(200).json({
             success: true,
@@ -80,7 +80,7 @@ exports.deleteBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-        if (!bootcamp) return res.status(400).json({ success: false });
+        if (!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 
         res.status(200).json({
             success: true,
