86c7c2b40bc00d2621dce6057ab6f8d2f7722b09
Aleksey Belchenkov <u608110@gmail.com>
2020-01-09 07:40:16 +0300

    Course Router & Controllers

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+
 // Middleware
 //const logger = require('./middleware/logger');
 
@@ -33,6 +35,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
