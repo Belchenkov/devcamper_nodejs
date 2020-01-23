a796caafea4fb8d0190b7bcbd78a9c4c3d6110c5
Aleksey Belchenkov <u608110@gmail.com>
2020-01-23 06:19:06 +0300

    Fotgot Password - Generate Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,6 +44,38 @@ exports.login = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
+// @desc    Get current logged in User
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+// @desc    Forgot Password
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user)
+        return next(new ErrorResponse('There is no user with that email', 404));
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     // Create Token
@@ -57,22 +89,9 @@ const sendTokenResponse = (user, statusCode, res) => {
     if (process.env.NODE_ENV === 'production') options.secure = true;
 
     res.status(statusCode)
-       .cookie('token', token, options)
-       .json({
-           success: true,
-           token
-       });
+        .cookie('token', token, options)
+        .json({
+            success: true,
+            token
+        });
 };
-
-// @desc    Get current logged in User
-// @route   POST /api/v1/auth/me
-// @access  Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
-
-    res.status(200).json({
-        success: true,
-        data: user
-    });
-});
-
