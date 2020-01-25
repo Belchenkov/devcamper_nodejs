2ac7b3411154c18b5bb95e9d116b110a8fa699d2
Aleksey Belchenkov <u608110@gmail.com>
2020-01-25 10:12:22 +0300

    Send Email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,18 +10,16 @@ const sendEmail = async (options) => {
         }
     });
 
-    // send mail with defined transport object
-    let message = {
+    const message = {
         from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
         to: options.email,
         subject: options.subject,
         text: options.message
     };
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    const info = await transporter.sendMail(message);
 
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    console.log("Message sent: %s", info.messageId);
 };
+
+module.exports = sendEmail;
